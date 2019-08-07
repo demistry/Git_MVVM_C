05991566abffffd912d04fdf1dd7e439b59115f4
David Ilenwabor <dave11ilen@gmail.com>
2019-08-07 02:12:30 +0100

    Initial Commit

---
+++ Git_MVVM_CTests/Git_MVVM_CTests.swift
@@ -0,0 +1,34 @@
+//
+//  Git_MVVM_CTests.swift
+//  Git_MVVM_CTests
+//
+//  Created by David Ilenwabor on 07/08/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import XCTest
+@testable import Git_MVVM_C
+
+class Git_MVVM_CTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
